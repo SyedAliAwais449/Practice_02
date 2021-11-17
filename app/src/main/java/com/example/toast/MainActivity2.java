5cb976834bbe25593d0e1c4d6c837098c9b030cf
SyedAliAwais449 <saliawaisd449@gmail.com>
2021-11-17 19:35:52 +0500

    onItemClickListner & Toast

--- app/src/main/java/com/example/toast/MainActivity2.java
+++ app/src/main/java/com/example/toast/MainActivity2.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class MainActivity2 extends AppCompatActivity {
     ArrayList<String> arrayList;
@@ -26,13 +30,22 @@ public class MainActivity2 extends AppCompatActivity {
         arrayList.add("Saad");
         arrayList.add("Haseeb");
         arrayList.add("Ahmad");
+        arrayList.add("Babar");
+        Collections.sort(arrayList);
         adapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity2.this, arrayList.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     public void onClick5(View v){
-        if(editText.getText().toString()!= null){
+        if(!editText.getText().toString().equals("")){
             arrayList.add(editText.getText().toString());
             adapter.notifyDataSetChanged();
         }
     }
+
 }
